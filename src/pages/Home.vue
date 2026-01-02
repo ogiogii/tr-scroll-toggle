27888876ae1287b9487cd6c97bfb29da23351c5c
ogiogii <riswandiyhogi@gmail.com>
2026-01-02 23:23:47 +0700

    Base layout with header and sections

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <SectionOne />
+    <SectionTwo />
+    <SectionThree />
+  </main>
+</template>
+
+<script>
+import SectionOne from '@/components/SectionOne.vue'
+import SectionTwo from '@/components/SectionTwo.vue'
+import SectionThree from '@/components/SectionThree.vue'
+
+export default {
+  components: {
+    SectionOne,
+    SectionTwo,
+    SectionThree
+  }
+}
+</script>
